a49809a6605c5c66f6e9f98af28b10ffa7e0c76c
Aardvark <highcrypto098@gmail.com>
2025-02-11 10:35:27 +0900

    feat: increase performance

--- hard/death-first-search-episode-2.cpp
+++ hard/death-first-search-episode-2.cpp
@@ -2,90 +2,252 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <map>
 #include <queue>
-#include <stdio.h>
+#include<bits/stdc++.h>
+
+#define NODE_NOT_FOUND      (-1)
+#define INF 0x3f3f3f3f
 
 using namespace std;
-struct Node {
-  set<int> links;
-  bool gateways;
-  set<int> tGatewayLinks;
-};
 
-pair<int, int> bfs(vector<Node>& graph, int s) {
-  vector<bool> visited(graph.size(), false);
-  queue<int> Q;
-  Q.push(s);
-  visited[s] = true;
+typedef pair<int, int> iPair;
 
-  int selectedNode = -1;
-
-  while(!Q.empty()){
-    int id = Q.front();
-    Q.pop();
-    Node n = graph[id];
-    visited[id] = true;
-
-    const auto push_neighbours = [&n, &Q, &visited]() {
-      for (const int& nid : n.links) {
-        if (not visited[nid]) Q.push(nid);
-      }
-    };
-
-    if(n.tGatewayLinks.size() > 1) {
-      selectedNode = id;
-      break;
-    }else if (n.tGatewayLinks.size() == 1) {
-      if(selectedNode == -1) {
-        selectedNode = id;
-        cerr << "selectedNode: " << id;
-        if (id == s) break;
-      } 
-      push_neighbours();
+class Edge {
+public:
+    Edge(int _v, int _u, int _w = 1) : v(_v), u(_u), w(_w) { served = true; }
+    bool exitEdge(int e) { 
+        if (v == e || u == e) {
+            served = false;
+            return true;
+        }
+        return false;
     }
-    else if (selectedNode == -1) push_neighbours();
-
-  }
-
-  return make_pair(selectedNode, *(graph[selectedNode].tGatewayLinks.begin()));
-
+    void setWeight(int _w) { w = _w;        }
+    void setServed()       { served = true; }
+    
+    int    getFrom() const { return v;      }
+    int      getTo() const { return u;      }
+    int  getWeight() const { return w;      }
+    bool getServed() const { return served; }
+    
+   
+    bool operator!=(const Edge &e) { return !(v == e.v && u == e.u); }
+    
+    friend bool operator==(const Edge &current, const Edge &data);
+    friend bool operator<(const Edge &e1, const Edge &e2);
+    friend ostream& operator<<(ostream &stream, const Edge &e);
+    friend Edge verifyEdge(const Edge &current, const Edge &data);
+    
+    int v, u, w;
+    bool served;
+};
+Edge noneEdge(-1, -1, -1);
+bool operator<(const Edge &e1, const Edge &e2) {
+    if (e1.v < e2.v) return true;
+    else if (e1.v == e2.v && e1.u < e2.u) return true;
+    else return false;
+}
+ostream& operator<<(ostream &stream, const Edge &e) {
+    stream << e.v << " --> " << e.u << "  s| " << e.served << "  w| " << e.w;
+    return stream;
+}
+bool operator==(const Edge &current, const Edge &data) {
+    return (current.v == data.v && current.u == data.u); 
+}
+Edge verifyEdge(const Edge &current, const Edge &data) {
+    if (current == data) return data;
+    else return noneEdge;
 }
 
+class Graph
+{
+    int V; 
+    list< pair<int, int> > *adj;
+ 
+public:
+    Graph(int V);
+    void addEdge(int v, int u, int w);
 
-int main() {
-  int N;
-  int L;
-  int E;
+    int shortestPath(int s, vector<int> &f);
+};
+ 
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<iPair> [V];
+}
+void Graph::addEdge(int v, int u, int w) {
+    adj[v].push_back(make_pair(u, w));
+    adj[u].push_back(make_pair(v, w));
+}
+int Graph::shortestPath(int src, vector<int> &first_target) {
+    
+    priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+    vector<int> dist(V, INF);
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+ 
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+        list< pair<int, int> >::iterator i;
+        
+        for (i = adj[u].begin(); i != adj[u].end(); ++i) {
+            int v = (*i).first;
+            int weight = (*i).second;
+ 
+            if (dist[v] > dist[u] + weight) {
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+ 
+    //cerr << "Vertex   Distance from Source\n";
+    map<int, int> to_print;
+    for (int i = 0; i < V; ++i) {
+        //cerr << i << " \t " << dist[i] << endl;
+        for (int j = 0; j < first_target.size(); ++j) 
+            to_print.insert(pair<int, int>(dist[first_target[j]], first_target[j]));
+        
+    }
+    auto it = to_print.begin();
+    return it->second;
+}
+ 
 
-  scanf("%d%d%d", &N, &L, &E);
+int main()
+{
+    int N; // the total number of nodes in the level, including the gateways
+    int L; // the number of links
+    int E; // the number of exit gateways
+    cin >> N >> L >> E; cin.ignore();
+    
+    vector<Edge> edge;
+    vector<int> Vertex, data_exit;
 
-  vector<Node> graph(N, {set<int>(), false, set<int>()});
-
-  for (int i = 0; i < L; i++){
-    int N1, N2;
-    scanf("%d%d", &N1, &N2);
-    graph[N1].links.insert(N2);
-    graph[N2].links.insert(N1);
-  }
-
-  for (int i = 0; i < E; i++) {
-    int EI;
-    scanf("%d", &EI);
-    graph[EI].gateways = true;
-    for (const int& nid: graph[EI].links) graph[nid].tGatewayLinks.insert(EI);
-  }
-
-  while(true) {
-    int st;
-    scanf("%d", &st);
-
-    pair<int, int> link = bfs(graph, st);
-    printf("%d %d\n", link.first, link.second);
-    graph[link.first].tGatewayLinks.erase(link.second);
-    graph[link.first].links.erase(link.second);
-    graph[link.second].links.erase(link.first);
-
-  }
-
+    for (int i = 0; i < L; i++) {
+        int N1, N2; // N1 and N2 defines a link between these nodes
+        cin >> N1 >> N2; cin.ignore();
+        
+        edge.push_back(Edge(N1, N2));
+        edge.push_back(Edge(N2, N1));
+        Vertex.push_back(N2);
+        Vertex.push_back(N1);
+    }
+    sort(edge.begin(), edge.end());
+    sort(Vertex.begin(), Vertex.end());
+    auto it = unique(Vertex.begin(), Vertex.end());
+    Vertex.resize(distance(Vertex.begin(), it));
+    
+    int V = Vertex.size();
+    
+    for (int i = 0; i < E; i++) {
+        int EI; // the index of a gateway node
+        cin >> EI; cin.ignore();
+        
+        data_exit.push_back(EI);
+        for (auto it = edge.begin(); it != edge.end(); ++it) {
+            it->exitEdge(EI);
+        }
+        Vertex.erase(find(Vertex.begin(), Vertex.end(), EI));
+    }
+    
+    vector<int> first_target, for_weight;
+    for (int i = 0; i < Vertex.size(); i++) {
+        int count = 0;
+        for (auto it = edge.begin(); it != edge.end(); ++it) {
+            if (it->v == Vertex[i]) {
+                for (int j = 0; j < data_exit.size(); ++ j) {
+                    if (it->u == data_exit[j]) {
+                        ++count;
+                        for_weight.push_back(Vertex[i]);
+                    }
+                }
+                
+                if (count > 1) first_target.push_back(Vertex[i]);
+            }
+        }
+    }
+    
+    sort(for_weight.begin(), for_weight.end());
+    auto iw = unique(for_weight.begin(), for_weight.end());
+    for_weight.resize(distance(for_weight.begin(), iw));
+    
+    sort(first_target.begin(), first_target.end());
+    auto ip = unique(first_target.begin(), first_target.end());
+    first_target.resize(distance(first_target.begin(), ip));
+    
+    for (int i = 0; i < edge.size(); i++) {
+        for (int j = 0; j < for_weight.size(); j++) {
+            if (edge[i].v == for_weight[j]) {
+                for (int k = 0; k < for_weight.size(); k++) {
+                    if (edge[i].u == for_weight[k]) edge[i].setWeight(0);
+                }
+            }
+        }
+    }
+    
+    Graph graph(V);
+    for(auto &it: edge) {
+        //cerr << it << endl;
+        graph.addEdge(it.getFrom(), it.getTo(), it.getWeight());
+    }
+    
+    while (1) {
+        int SI; // The index of the node on which the Skynet agent is positioned this turn
+        cin >> SI; cin.ignore();
+        
+        bool find_edge = false;
+        for (int i = 0; i < data_exit.size(); ++i) {
+            Edge current(SI, data_exit[i]);
+            for (int j = 0; j < edge.size(); ++j) {
+                if (verifyEdge(current, edge[j]) != noneEdge && !edge[j].getServed()) {
+                    cout << edge[j].getFrom() << " " << edge[j].getTo() << endl;
+                    edge[j].setServed();
+                    auto it = find(edge.begin(), edge.end(), Edge(data_exit[i], SI));
+                    it->setServed();
+                    find_edge = true;
+                    break;
+                }
+            }
+            if (find_edge) break;
+        }
+    
+        if (find_edge) continue;
+        else if (!find_edge && !first_target.empty()) {
+            int print_vertex = graph.shortestPath(SI, first_target);
+        
+            for (int i = 0; i < data_exit.size(); ++i) {
+                Edge current(print_vertex, data_exit[i]);
+                bool find_current = false;
+                for (int j = 0; j < edge.size(); ++j) {
+                    if (verifyEdge(current, edge[j]) != noneEdge && !edge[j].getServed()) {
+                        cout << edge[j].getFrom() << " " << edge[j].getTo() << endl;
+                        edge[j].setServed();
+                        auto it = find(edge.begin(), edge.end(), Edge(data_exit[i], print_vertex));
+                        it->setServed();
+                        first_target.erase(find(first_target.begin(), first_target.end(), print_vertex));
+                        find_current = true;
+                        break;
+                    }
+                }
+                if (find_current) break;
+            }
+        }
+        else {
+            for (int i = 0; i < edge.size(); ++i) {
+                if(!edge[i].getServed()) {
+                    cout << edge[i].getFrom() << " " << edge[i].getTo() << endl;
+                    edge[i].setServed();
+                    auto it = find(edge.begin(), edge.end(), Edge(edge[i].getTo(), edge[i].getFrom()));
+                    it->setServed();
+                    break;
+                }
+                
+            }
+        }
+    }
+    
 }
--- hard/death-first-search-episode-2.rs
+++ hard/death-first-search-episode-2.rs
@@ -1,102 +1,281 @@
 use std::collections::{HashSet, VecDeque};
-use std::io::{self, BufRead};
+use std::io;
+use std::borrow::BorrowMut;
+use std::collections::hash_set::Iter;
 
-#[derive(Default, Clone)]
-struct Node {
-  links: HashSet<usize>,
-  gateway: bool,
-  tgwlinks: HashSet<usize>,
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
-fn bfs(graph: &mut Vec<Node>, s: usize) -> (usize, usize) {
-  let mut visited = vec![false; graph.len()];
-  let mut q = VecDeque::new();
-  q.push_back(s);
-  visited[s] = true;
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let n = parse_input!(inputs[0], i32); // the total number of nodes in the level, including the gateways
+    let l = parse_input!(inputs[1], i32); // the number of links
+    let e = parse_input!(inputs[2], i32); // the number of exit gateways
 
-  let mut selected_node = None;
+    let mut graph: Vec<HashSet<i32>> = (0..n).into_iter()
+        .map(|_| -> HashSet<i32> {HashSet::new()})
+        .collect();
+    // let mut vertices:Vec<HashSet<i32>> = Vec::with_capacity(n as usize);
+    let mut exit_list: Vec<i32> = Vec::new();
 
-  while let Some(id) = q.pop_front() {
-    let n = &graph[id];
-    visited[id] = true;
 
-    let mut push_neighbours = || {
-      for &nid in &n.links {
-        if !visited[nid] {
-          q.push_back(nid);
-        }
-      }
+    for i in 0..l as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let n1 = parse_input!(inputs[0], i32); // N1 and N2 defines a link between these nodes
+        let n2 = parse_input!(inputs[1], i32);
+        // let n1u: usize = n1;
+        graph[n1 as usize].insert(n2);
+        graph[n2 as usize].insert(n1);
+    }
+    for i in 0..e as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let ei = parse_input!(input_line, i32); // the index of a gateway node
+        exit_list.push(ei);
+    }
+
+    // println!("{:?}", graph);
+    // return ();
+
+    let mut network = Network {
+        graph: graph,
+        exits: exit_list.clone(),
     };
 
-    if n.tgwlinks.len() > 1 {
-      selected_node = Some(id);
-      break;
-    } else if n.tgwlinks.len() == 1 {
-      if selected_node.is_none() {
-        selected_node = Some(id);
-        eprint!("selectedNode: {}\n", id);
-        if id == s {
-          break;
-        }
-      }
-      push_neighbours();
-    } else if selected_node.is_none() {
-      push_neighbours();
-    }
-  }
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let si = parse_input!(input_line, i32); // The index of the node on which the Skynet agent is positioned this turn
 
-  let selected_node = selected_node.unwrap();
-  (selected_node, *graph[selected_node].tgwlinks.iter().next().unwrap())
+        let edge = get_best_exit(&network, &exit_list, si);
+
+        println!("{} {}", edge.0, edge.1);
+
+        network.remove_edge(edge);
+    }
 }
 
-fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
+struct Node {
+    index: i32,
+    distance_from_virus: i32,
+    adjacent_exits_number: i32,
+    priority: i32,
+}
 
-    let first_line = lines.next().unwrap().unwrap();
-    let mut parts = first_line.split_whitespace();
-    let n: usize = parts.next().unwrap().parse().unwrap();
-    let l: usize = parts.next().unwrap().parse().unwrap();
-    let e: usize = parts.next().unwrap().parse().unwrap();
+impl Node {
+    fn new(index: i32, network: &Network, paths: &Vec<i32>, si: i32) -> Node {
+        let distance_from_virus = get_path_length(&paths, index, si);
+        let adjacent_exits_number = network.get_red_edges_count_for_node(index);
 
-    let mut graph = vec![Node::default(); n];
+        let path = Paths::get_path_for_node_2(paths, index);
+        let red_edges_count = network.get_red_edges_count_for_path(&path);
 
-    for _ in 0..l {
-        let line = lines.next().unwrap().unwrap();
-        let mut parts = line.split_whitespace();
-        let n1: usize = parts.next().unwrap().parse().unwrap();
-        let n2: usize = parts.next().unwrap().parse().unwrap();
-        graph[n1].links.insert(n2);
-        graph[n2].links.insert(n1);
+        Node {
+            index,
+            distance_from_virus,
+            adjacent_exits_number,
+            // priority: distance_from_virus - adjacent_exits_number - 1,
+            priority: red_edges_count - path.len() as i32,
+        }
     }
+}
 
-    let mut gateways = Vec::new();
-    for _ in 0..e {
-        let line = lines.next().unwrap().unwrap();
-        let ei: usize = line.parse().unwrap();
-        graph[ei].gateway = true;
-        gateways.push(ei);
-    }
+fn get_best_exit(network: &Network, exits: &Vec<i32>, si: i32) -> (i32, i32) {
+    let (_paths, mut nodes) = create_paths_and_nodes(&network, &exits, si);
 
-    for &ei in &gateways {
-        let links: Vec<usize> = graph[ei].links.iter().cloned().collect();
-        for nid in links {
-            graph[nid].tgwlinks.insert(ei);
+    nodes.sort_by(|a, b| {
+        if a.priority != b.priority {
+            return b.priority.cmp(&a.priority);
+        }
+
+        // if a.adjacent_exits_number != b.adjacent_exits_number {
+        //     return b.adjacent_exits_number.cmp(&a.adjacent_exits_number);
+        // }
+
+        return a.distance_from_virus.cmp(&b.distance_from_virus);
+    });
+
+    let most_prioritized_hub = nodes[0].index;
+    let exit: i32 = network.iter_neighbors(most_prioritized_hub)
+        .find(|n| exits.contains(n))
+        .unwrap().clone();
+
+    (most_prioritized_hub, exit)
+}
+
+fn create_paths_and_nodes(network: &Network, exits: &Vec<i32>, si: i32) -> (Paths, Vec<Node>) {
+    let mut visited: HashSet<i32> = HashSet::new();
+    let mut paths: Vec<i32> = vec![-1; network.graph.len()];
+    let mut queue: VecDeque<i32> = VecDeque::new();
+
+    let mut nodes: Vec<Node> = Vec::new();
+
+    queue.push_back(si);
+
+    while !queue.is_empty() {
+
+        let current = queue.pop_front().unwrap();
+        visited.insert(current);
+        if !exits.contains(&current) {
+            let node = Node::new(current, &network, &paths, si);
+            if node.adjacent_exits_number > 0 {
+                nodes.push(node);
+            }
+        }
+
+
+        for &neighbor in network.iter_neighbors(current) {
+            if exits.contains(&neighbor) {
+                continue;
+            }
+
+            if !visited.contains(&neighbor) {
+                paths[neighbor as usize] = current;
+                queue.push_back(neighbor);
+                visited.insert(neighbor);
+            }
         }
     }
 
+    (Paths {parents: paths}, nodes)
+}
+
+fn get_path_length(paths: &Vec<i32>, exit: i32, si: i32) -> i32 {
+    let mut length = 0;
+    let mut current = exit;
     loop {
-        let line = lines.next().unwrap().unwrap();
-        let si: usize = line.parse().unwrap();
+        let parent = paths[current as usize];
 
-        let (node, gateway) = bfs(&mut graph, si);
-        println!("{} {}", node, gateway);
+        if parent == -1 {
+            return 0;
+        }
 
-        // Cut link:
-        graph[node].tgwlinks.remove(&gateway);
-        graph[node].links.remove(&gateway);
-        graph[gateway].links.remove(&node);
+        current = parent;
+        length += 1;
+
+        if parent == si {
+            break;
+        }
     }
 
+    length
+}
 
+struct Network {
+    graph: Vec<HashSet<i32>>,
+    exits: Vec<i32>,
+}
+
+impl Network {
+    fn remove_edge(&mut self, edge: (i32, i32)) {
+        self.graph[edge.0 as usize].remove(&edge.1);
+        self.graph[edge.1 as usize].remove(&edge.0);
+    }
+
+    fn get_red_edges_count_for_path(&self, path: &Vec<i32>) -> i32 {
+        let mut count = 0;
+
+        for &node in path {
+            count += self.get_red_edges_count_for_node(node);
+        }
+
+        count
+    }
+
+    fn get_red_edges_count_for_node(&self, node: i32) ->i32 {
+        // self.graph[node as usize].iter().filter(|n| {
+        //     self.exits.contains(n)
+        // }).count() as i32
+        self.iter_neighbors(node)
+            .filter(|n| {
+                self.exits.contains(n)
+            })
+            .count() as i32
+    }
+
+    fn iter_neighbors(&self, node: i32) -> Iter<i32> {
+        self.graph[node as usize].iter()
+    }
+}
+
+struct Paths {
+    parents: Vec<i32>,
+}
+
+impl Paths {
+    fn get_path_for_node(&self, node: i32) -> Vec<i32> {
+        let mut path: Vec<i32> = Vec::new();
+        let mut current = node;
+        path.push(node);
+
+        loop {
+            let parent = self.parents[current as usize];
+            if parent == -1 {
+                break;
+            }
+            path.push(parent);
+            current = parent;
+        }
+
+        path
+    }
+
+    fn get_path_for_node_2(parents: &Vec<i32>, node: i32) -> Vec<i32> {
+        let mut path: Vec<i32> = Vec::new();
+        let mut current = node;
+        path.push(node);
+
+        loop {
+            let parent = parents[current as usize];
+            if parent == -1 {
+                break;
+            }
+            path.push(parent);
+            current = parent;
+        }
+
+        path
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod parent_to_path {
+        use super::*;
+
+        #[test]
+        fn test_single_node() {
+            let paths = Paths {
+                parents: vec![-1, 0, 1]
+            };
+
+            let path = paths.get_path_for_node(0);
+
+            assert_eq!(path, vec![0]);
+        }
+
+        #[test]
+        fn test_three_nodes() {
+            let paths = Paths {
+                parents: vec![-1, 0, 1]
+            };
+
+            let path = paths.get_path_for_node(2);
+
+            assert_eq!(path, vec![2, 1, 0]);
+        }
+    }
 }
